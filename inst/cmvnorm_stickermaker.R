fbc1360068f70fa783bf087642dd2d607b2c1449
Robin Hankin <hankin.robin@gmail.com>
2021-09-18 15:17:20 +1200

    sticker

---
+++ inst/cmvnorm_stickermaker.R
@@ -0,0 +1,14 @@
+# reproduces a slight modification of figure 18.5 (top), p643.
+
+library("hexSticker")
+
+sticker("cmvnorm_icon.png", package="cmvnorm", p_size=20, s_x=1, s_y=1,
+s_width=0.8,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+h_color="#000000", filename="cmvnorm.png")
+
+
+
+sticker("cmvnorm_icon.png", package="cmvnorm", p_size=20, s_x=1, s_y=0.7,
+        s_width=0.6,asp=sqrt(3)/2, white_around_sticker=TRUE, h_fill="#7733FF",
+        h_color="#000000", filename="cmvnorm1.png")
+
